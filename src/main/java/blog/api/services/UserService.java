7ece403652c96c14bd78df0f9d85545b8bc55bbf
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2024-04-11 10:24:14 -0300

    feat: Added the User service file

---
+++ src/main/java/blog/api/services/UserService.java
@@ -0,0 +1,12 @@
+package blog.api.services;
+
+import blog.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository repository;
+}
