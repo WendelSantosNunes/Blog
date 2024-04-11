5d9a8f3c79ffa40445a7ab712a8e1345e98bc024
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2024-04-11 10:32:52 -0300

    feat: Adding the UserController file

---
+++ src/main/java/blog/api/controllers/UserController.java
@@ -0,0 +1,14 @@
+package blog.api.controllers;
+
+import blog.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class UserController {
+
+    @Autowired
+    UserService service;
+}
